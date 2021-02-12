d8629d095b64af99c1c03f2ce0b7f0106fa8d151
Gone Raghukumar <raghukumar.gone@dxc.com>
2021-02-12 15:43:39 +0530

    made changes

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,7 @@
+import os
+
 DEBUG = False
 SECRET_KEY = 'topscret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:qwer@localhost/catlog_db'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:qwer@localhost/catlog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
